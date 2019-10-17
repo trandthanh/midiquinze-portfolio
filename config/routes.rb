37d2365a6edc7d25851a44843d7c503fd2659061
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-17 11:19:39 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  get 'pages/contact'
+  get 'pages/about'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
