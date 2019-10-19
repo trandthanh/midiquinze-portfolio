06f0f5d4c216826ec5643512bd473cf5d4dcb4c8
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-19 22:22:47 +0200

    starting navbar

--- app/javascript/components/loader.js
+++ app/javascript/components/loader.js
@@ -2,6 +2,7 @@ const loader = () => {
   document.querySelector(".loader").addEventListener('animationend', (event) => {
     document.getElementById("midiquinze-gif-loader").classList.add("display-none");
     document.getElementById("home").classList.remove("display-none");
+    document.getElementById("home").classList.add("display-block");
   })
 }
 
