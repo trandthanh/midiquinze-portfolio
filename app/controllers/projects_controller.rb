777bf3273497fa4af49520726aa05e50a955b625
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-28 11:57:31 +0100

    streamlined color

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,34 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_color, only: [:index]
 
   def index
     @projects = Project.all
   end
 # params.require(:product).permit(:title, :description, images: [])
+
+  def change_color
+    if session[:color] == "black"
+      session[:color] = "white"
+      @colors = "background-color-black color-white"
+      redirect_to request.referrer
+    else
+      session[:color] = "black"
+      @colors = "background-color-white color-black"
+      redirect_to request.referrer
+    end
+  end
+
+  private
+
+  def set_color
+    if session[:color].nil?
+      session[:color] = "black"
+      @colors = "background-color-black color-white"
+    elsif session[:color] == "black"
+      @colors = "background-color-black color-white"
+    else
+      @colors = "background-color-white color-black"
+    end
+  end
 end
