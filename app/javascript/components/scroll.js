c7d91159ee25ed4494b6728d0f3b0c9cd7016c30
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-29 11:48:53 +0100

    added JS name

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -1,23 +1,40 @@
 const scroll = () => {
   const slides = document.querySelectorAll(".slide");
+
+  // name
+  const changeName = (nextSlide) => {
+    let projectName = nextSlide.dataset.project;
+    let projectText = document.querySelector("#project-name");
+    let slash = document.querySelector("#slash");
+    projectText.innerText = projectName;
+    slash.innerText = "/";
+  }
+
   slides.forEach((slide) => {
     slide.addEventListener('wheel', (event) => {
 
-      const bodyRect = document.body.getBoundingClientRect();
+
+
+
 
       let navbarHeight = document.querySelector(".midiquinze-navbar").offsetHeight;
-      console.log(navbarHeight);
       let nextSlideNumber = parseInt(slide.dataset.number, 10) + 1;
-      console.log(nextSlideNumber);
       let nextSlide = document.querySelector(`#page-${nextSlideNumber}`);
-      console.log(nextSlide);
       let y = nextSlide.offsetTop - navbarHeight;
-      console.log(y);
+      // console.log(y);
 
+
+
+      // scroll
       window.scrollTo({
         top: y,
         behavior: 'smooth'
       })
+
+
+      setTimeout(() => changeName(nextSlide), 1000);
+
+
     })
   })
 }
