73f9e8bfd9fe91e60cf8010396b279c947e8df5b
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-30 15:09:58 +0100

    push sheed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cléo = Member.create(name: "Cléo Barand", "ART DIRECTOR")
+romain = Member.create(name: "ROMAIN BELLEVILLE", "CREATIVE DIRECTOR + CO-FOUNDER")
+jérémie = Member.create(name: "JÉRÉMIE BŒGLIN", "CREATIVE DIRECTOR + CO-FOUNDER")
+gwendoline = Member.create(name: "GWENDOLINE RAVET", "CLIENT DIRECTOR")
+laura = Member.create(name: "LAURA JOVELIN", "ART DIRECTOR JR")
+léa = Member.create(name: "LÉA RETAILLEAU", "ASSISTANT ART DIRECTOR")
