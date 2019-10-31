633744468aba48a40d1219a9556841e9a49083b8
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-31 18:07:21 +0100

    before installing js pack

--- app/javascript/components/clickScroll.js
+++ app/javascript/components/clickScroll.js
@@ -12,19 +12,48 @@ const clickScroll = () => {
 }
 
 const firstPageScroll = () => {
-  let alreadyFire = false;
-  const pageZero = document.querySelector(".page-0");
-  pageZero.addEventListener('mousewheel', (event) => {
-    // if (alreadyFire) { return null };
-    console.log(event)
-    event.preventDefault();
-    document.querySelector(".page-1").scrollIntoView({
-      behavior: 'smooth'
-    })
-    alreadyFire = true;
-    // je vais à l'ancre
+  const page0 = document.querySelector(".page-0");
+  let fired = false;
+  console.log(fired);
+  page0.addEventListener('wheel', (event) => {
+    if (fired) {
+      console.log(fired)
+    } else {
+      console.log('hi')
+      event.preventDefault();
+      document.querySelector(".page-1").scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+    fired = true;
   })
 }
 
 
-export { clickScroll, firstPageScroll };
+const withoutWheel = () => {
+  const test = () => {
+    // event.preventDefault();
+    document.querySelector(".page-1").scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
+  const no = () => {
+    const home = document.querySelector("#mq-home");
+    home.classList.remove("position-fixed");
+  }
+  const page0 = document.querySelector(".page-0");
+  page0.addEventListener('wheel', (event) => {
+    // if (window.scrollY > 0) {
+      console.log(event);
+      // setTimeout(no, 100);
+      event.preventDefault();
+      setTimeout(test, 20);
+    // } else {
+    //   console.log('no')
+    // }
+  })
+}
+
+
+export { clickScroll, firstPageScroll, withoutWheel };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,10 @@ import "bootstrap";
 
 import { loader } from "../components/loader";
 import { color } from "../components/color";
-import { clickScroll, firstPageScroll, scrollTo } from "../components/clickScroll";
+import { clickScroll, firstPageScroll, scrollTo, withoutWheel } from "../components/clickScroll";
 import { mouse } from "../components/mouse";
 import { overlay } from "../components/overlay";
+import { debounce } from "../components/debounce";
 
 
 color();
@@ -16,9 +17,10 @@ if (document.querySelector(".loader")) {
 
 if (document.querySelector(".page-0")) {
   clickScroll();
-  firstPageScroll();
+  // firstPageScroll();
   mouse();
 }
 
 
 overlay();
+withoutWheel();
