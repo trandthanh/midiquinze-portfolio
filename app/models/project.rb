fcf1519f1466660f548b857a6de527d2be7e99ea
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-01 16:21:59 +0100

    finished white edits

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,12 +5,14 @@ class Project < ApplicationRecord
   has_many :jobs, through: :works
 
   has_one_attached :cover
-  has_one_attached :logo
+  has_one_attached :logo_black
+  has_one_attached :logo_white
   has_one_attached :thumbnail
   has_many_attached :photos
 
   validates :cover, presence: true
-  validates :logo, presence: true
+  validates :logo_black, presence: true
+  validates :logo_white, presence: true
   validates :thumbnail, presence: true
   validates :photos, presence: true
 end
