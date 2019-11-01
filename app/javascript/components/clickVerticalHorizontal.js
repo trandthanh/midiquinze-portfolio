31dc417a89dac15e8de212b26480efd819920f3f
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-01 10:21:15 +0100

    finished scroll panel on home page

--- app/javascript/components/clickVerticalHorizontal.js
+++ app/javascript/components/clickVerticalHorizontal.js
@@ -4,68 +4,105 @@ const clickVerticalHorizontal = () => {
   const windowWidth = window.innerWidth;
   const numberOfProjects = parseInt(document.querySelector("#mq-home").dataset.projectCount, 10);
 
+  const scrollIntoViewSlide = (selector) => {
+    document.querySelector(selector).scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
+  const stopClickPropagation = (element) => {
+    element.addEventListener("click", (event) => {
+      event.stopPropagation(); //this is important! If removed, you'll get both alerts
+    }, false);
+  }
+
+  const stopOverlayPropagation = (openButton, closeButton, overlayArea) => {
+    stopClickPropagation(openButton);
+    stopClickPropagation(closeButton);
+    stopClickPropagation(overlayArea);
+  }
+
   slides.forEach((slide) => {
-    // last cover slide
+
+    const number = parseInt(slide.dataset.number, 10);
+    const nextNumber = number + 1;
+    const previousNumber = number - 1;
+
+    const project = slide.dataset.project;
+
+    const panoIndex = parseInt(slide.dataset.panoIndex, 10);
+    const nextPanoIndex = panoIndex + 1;
+    const previousPanoIndex = panoIndex - 1;
+
+    const button = slide.dataset.button;
+
+    const openBtn = document.querySelector(`#openbtn-${project}-${button}`);
+    const closeBtn = document.querySelector(`#closebtn-${project}-${button}`);
+    const overlay = document.querySelector(`#overlay-${project}-${button}`);
+
+    const numberOfSlides = parseInt(slide.dataset.panoCount, 10);
+
+    // last row cover
     if (slide == document.querySelector(`.cover${numberOfProjects}`)) {
-      slide.addEventListener('click', (event) => {
-        document.querySelector(".cover1").scrollIntoView({
-          behavior: 'smooth'
-        })
+
+      slide.addEventListener('click', (event) => { // click on last row cover
+
+        if (event.clientX < (windowWidth / 3)) { // left on the cover
+          // nothing to do
+        } else if (event.clientX > (windowWidth / 3 * 2)) { // right
+          scrollIntoViewSlide(`.pano${nextPanoIndex}-${project}`);
+        } else if (event.clientY < (windowHeight / 2)) { // up
+          scrollIntoViewSlide(`.cover${previousNumber}`);
+        } else { // down
+          scrollIntoViewSlide(".cover1");
+        }
+
       })
-    } else {
+
+      stopOverlayPropagation(openBtn, closeBtn, overlay);
+
+    } else { // other slides
+
       slide.addEventListener('click', (event) => {
-        const number = parseInt(slide.dataset.number, 10);
-        const nextNumber = number + 1;
-        const previousNumber = number - 1;
 
-        const project = slide.dataset.project;
+        if (event.clientX < (windowWidth / 3)) { // left
 
-        const panoIndex = parseInt(slide.dataset.panoIndex, 10);
-        const nextPanoIndex = panoIndex + 1;
-        const previousPanoIndex = panoIndex - 1;
-
-        // left
-        if (event.clientX < (windowWidth / 3)) {
-          document.querySelector(`.pano${previousPanoIndex}-${project}`).scrollIntoView({
-            behavior: 'smooth'
-          })
-
-        // right
-        } else if (event.clientX > (windowWidth / 3 * 2)) {
-          // console.log('right');
-          // if (slide == document.querySelector(`.cover${number}`)) {
-          //   document.querySelector(`.pano0-${project}`).scrollIntoView({
-          //     behavior: 'smooth'
-          //   })
-          // } else {
-            document.querySelector(`.pano${nextPanoIndex}-${project}`).scrollIntoView({
-              behavior: 'smooth'
-            })
-          // }
-
-        // up
-        } else if (event.clientY < (windowHeight / 2)) {
-          if (slide == document.querySelector(`.cover${number}`)) {
-            document.querySelector(`.cover${previousNumber}`).scrollIntoView({
-              behavior: 'smooth'
-            })
-          } else {
-            console.log(`.cover${previousNumber}`);
-            document.querySelector(`.cover${previousNumber}`).scrollIntoView({
-              behavior: 'smooth'
-            })
+          if (slide == document.querySelector(`.cover${number}`)) { // left on the cover
+            // nothing to do
+          } else { // other slides
+            scrollIntoViewSlide(`.pano${previousPanoIndex}-${project}`);
           }
 
-        // down
-        } else {
-          document.querySelector(`.cover${nextNumber}`).scrollIntoView({
-            behavior: 'smooth'
-          })
-        }
-      })
+        } else if (event.clientX > (windowWidth / 3 * 2)) { // right
 
-    }
-  })
-}
+          if (panoIndex == numberOfSlides) { // last slide on row
+            scrollIntoViewSlide(`.cover${number}`);
+          } else { // other slides
+            scrollIntoViewSlide(`.pano${nextPanoIndex}-${project}`);
+          }
+
+        } else if (event.clientY < (windowHeight / 2)) { // up
+
+          scrollIntoViewSlide(`.cover${previousNumber}`);
+
+        } else { // down
+
+          if (number == numberOfProjects) { // last row slides except last row cover
+            scrollIntoViewSlide(".cover1");
+          } else { // other slides
+            scrollIntoViewSlide(`.cover${nextNumber}`);
+          }
+
+        } // if
+
+      }) // eventListener
+
+      stopOverlayPropagation(openBtn, closeBtn, overlay);
+
+    } // last row cover vs others
+
+  }) // listeners on each slide
+
+} // function
 
 export { clickVerticalHorizontal }
