741948b321bdc2c1ab23a8ef17841546ee9e5bb1
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-06 00:13:25 +0100

    background black on overlay

--- app/javascript/components/mobileOverlay.js
+++ app/javascript/components/mobileOverlay.js
@@ -2,11 +2,13 @@ const mobileOverlay = () => {
   /* Open when someone clicks on the span element */
   function openNav() {
     document.getElementById("midiquinze-mobile-navbar-overlay").style.height = "100%";
+    document.querySelector(".midiquinze-navbar-mobile").classList.add("background-color-black");
   }
 
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
     document.getElementById("midiquinze-mobile-navbar-overlay").style.height = "0%";
+    document.querySelector(".midiquinze-navbar-mobile").classList.remove("background-color-black");
   }
 
   const mobileOpenButton = document.querySelector("#burger-openbtn");
