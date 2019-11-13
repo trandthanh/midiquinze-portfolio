c1b283249c8d153bb617917c0d687fc591d6b7a1
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-13 17:51:27 +0100

    refacto navbar with helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,3 +1,13 @@
 module NavigationHelper
-
+  def if_current_page_navbar
+    if current_page?(projects_path)
+      render partial: "shared/navbar/desktop/desktop_navbar_projects", locals: { opposite_color: @opposite_color }
+    elsif current_page?(about_path) && params[:from].nil?
+      render partial: "shared/navbar/desktop/desktop_navbar_about", locals: { opposite_color: @opposite_color }
+    elsif current_page?(about_path) && params[:from] == "contact"
+      render partial: "shared/navbar/desktop/desktop_navbar_contact", locals: { opposite_color: @opposite_color }
+    else
+      render partial: "shared/navbar/desktop/desktop_navbar_root", locals: { opposite_color: @opposite_color }
+    end
+  end
 end
