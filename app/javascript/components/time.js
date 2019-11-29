d341dd2a3d79ccf16cb02c8eab123bd34f760010
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-30 00:08:44 +0100

    time edits depending on 0

--- app/javascript/components/time.js
+++ app/javascript/components/time.js
@@ -2,8 +2,20 @@ const time = () => {
 
   const updateClock = () => {
     let now = new Date();
-    if (now.getSeconds() < 10) {
+    if (now.getSeconds() < 10 && now.getMinutes() > 10 && now.getHours() > 10) {
       let time = now.getHours() + ':' + now.getMinutes() + ':' + '0' + now.getSeconds();
+      document.getElementById('time').innerHTML = [time].join(' / ');
+    } else if (now.getMinutes() < 10 && now.getSeconds() < 10 && now.getHours() > 10) {
+      let time = now.getHours() + ':' + '0' + now.getMinutes() + ':' + '0' + now.getSeconds();
+      document.getElementById('time').innerHTML = [time].join(' / ');
+    } else if (now.getMinutes() < 10 && now.getSeconds() < 10 && now.getHours() < 10) {
+      let time = '0' + now.getHours() + ':' + '0' + now.getMinutes() + ':' + '0' + now.getSeconds();
+      document.getElementById('time').innerHTML = [time].join(' / ');
+    } else if (now.getMinutes() > 10 && now.getSeconds() < 10 && now.getHours() > 10) {
+      let time = now.getHours() + ':' + '0' + now.getMinutes() + ':' + now.getSeconds();
+      document.getElementById('time').innerHTML = [time].join(' / ');
+    } else if (now.getMinutes() < 10 && now.getSeconds() > 10 && now.getHours() < 10) {
+      let time = '0' + now.getHours() + ':' + '0' + now.getMinutes() + ':' + now.getSeconds();
       document.getElementById('time').innerHTML = [time].join(' / ');
     } else {
       let time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
