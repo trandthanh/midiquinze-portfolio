85c3d2601753ef2cfeb2236cc495aae5df205a7f
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-12-05 23:53:50 +0100

    just moved footer up a bit

--- app/javascript/components/mobileOverlay.js
+++ app/javascript/components/mobileOverlay.js
@@ -1,7 +1,7 @@
 const mobileOverlay = () => {
   /* Open when someone clicks on the span element */
   function openNav() {
-    document.getElementById("midiquinze-mobile-navbar-overlay").style.height = "100vh";
+    document.getElementById("midiquinze-mobile-navbar-overlay").style.height = "95vh";
     // document.querySelector(".midiquinze-navbar-mobile").classList.add("background-color-black");
   }
 
